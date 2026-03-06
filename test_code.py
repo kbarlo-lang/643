c7b7c12eed8f4fa6f567fd1333fe84e2d614965d
Krystian <kbarlo@umich.edu>
2026-03-06 15:51:39 +0100

    This is the first draft of the test code.

---
+++ test_code.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def something():
+    df = pd.DataFrame()
+    return df
+
+def something_else():
+    temp = 50
+    other_var = 30
+    test_var = 10
+
+    if temp > 50: 
+        print("temp is > 50")
+    else:
+        print("want to see just for testing")
+        print("temp is less or equal to 50")
+
+    
+
+something_else()
